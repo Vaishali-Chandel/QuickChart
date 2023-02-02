642a706c5a44118585aac992792166175cd1366c
Vaishali-Chandel <101197682+Vaishali-Chandel@users.noreply.github.com>
2023-02-02 22:18:03 +0530

    Create Chart.java

---
+++ Chart.java
@@ -0,0 +1,21 @@
+public class Chart {
+
+    @Getter
+    @Setter
+    private String text;
+
+    @Getter
+    @Setter
+    private Double agreePer;
+
+    @Getter
+    @Setter
+    private Double disagreePer;
+
+    @Getter
+    @Setter
+    private Double neutralPer;
+
+    public Chart(){ super();}
+
+}
